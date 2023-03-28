bfffdc4bd3f0885b5a3fb52e1af97fa484d5fff5
huisso <chosohi@naver.com>
2023-03-28 15:55:01 +0900

    feat: add label to button

--- color-button/src/App.js
+++ color-button/src/App.js
@@ -4,10 +4,13 @@ import { useState } from 'react';
 
 function App() {
   const [style, setStyle] = useState('red');
+  const [disabled, setDisabled] = useState(false);
+
   const newStyle = style === 'red' ? 'blue' : 'red';
   return (
     <div>
       <button
+        disabled={disabled}
         onClick={() => {
           setStyle(newStyle);
         }}
@@ -15,6 +18,15 @@ function App() {
       >
         Change to {newStyle}
       </button>
+      <input
+        type="checkbox"
+        id="disable-button-checkbox"
+        defaultChecked={disabled}
+        onChange={(e) => {
+          setDisabled(e.target.checked);
+        }}
+      />
+      <label htmlFor="disable-button-checkbox">Disable button</label>
     </div>
   );
 }
--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -18,3 +18,28 @@ test('button has correct initial color, and updates when clicked', () => {
   // expect the button text to be "Change to Red"
   expect(colorButton).toHaveTextContent('Change to red');
 });
+
+test('initail conditions', () => {
+  render(<App />);
+  // check that the button starts out enabled
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+  expect(colorButton).toBeEnabled();
+  // check that the checkbox starts out unchecked
+  const checkbox = screen.getByRole('checkbox');
+  expect(checkbox).not.toBeChecked();
+
+  fireEvent.click(checkbox);
+  expect(colorButton).toBeDisabled();
+});
+
+test('Checkbox disables button on first click and enables on second click', () => {
+  render(<App />);
+
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+  const checkbox = screen.getByRole('checkbox', { name: 'Disable button' });
+
+  fireEvent.click(checkbox);
+  expect(colorButton).toBeDisabled();
+  fireEvent.click(checkbox);
+  expect(colorButton).toBeEnabled();
+});
